51ab2c74435700e84f995f175307d241822f5ac1
doloiEoh <do.loi@eoh.io>
2024-07-30 17:01:35 +0700

    fix add token

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,17 +2,26 @@ import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import { theme } from "../utils/utils";
+import { setAxiosDefaultAuthToken, theme } from "../utils/utils";
 import { Provider } from "react-redux";
 import store from "../redux/store";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 const MyApp = ({ Component, pageProps }) => {
+
   useEffect(() => {
     const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setAxiosDefaultAuthToken(token);
     }
   }, []);
 
